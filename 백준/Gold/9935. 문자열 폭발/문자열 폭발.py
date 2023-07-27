9183b50622acb02feff02529ee2a20dc28abb260
sskkilm <87752078+sskkilm@users.noreply.github.com>
2023-07-27 17:23:31 +0900

    [Gold IV] Title: 문자열 폭발, Time: 444 ms, Memory: 42436 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -4,10 +4,9 @@ str2 = input()
 stack = []
 for s in str1:
     stack.append(s)
-    if s == str2[len(str2)-1]:
-        if len(stack)-len(str2) >= 0 and ''.join(stack[len(stack)-len(str2):len(stack)])==str2:
-            for _ in range(len(str2)):
-                stack.pop()
+    if s == str2[-1] and ''.join(stack[-len(str2):])==str2:
+        for _ in range(len(str2)):
+            stack.pop()
 
 
 if not stack:
