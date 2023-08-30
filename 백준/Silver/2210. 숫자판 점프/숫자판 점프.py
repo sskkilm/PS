ccb4478db59a071fb25032e132ace80696c05d33
sskkilm <87752078+sskkilm@users.noreply.github.com>
2023-08-30 14:53:16 +0900

    [Silver II] Title: 숫자판 점프, Time: 608 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2210. 숫자판 점프/숫자판 점프.py
@@ -0,0 +1,29 @@
+# import sys
+# sys.setrecursionlimit(10**6)
+
+board = [list(map(str, input().split())) for _ in range(5)]
+
+
+answer = []
+delta = {(-1, 0), (1, 0), (0, -1), (0, 1)}
+
+def dfs(x, y, num):
+    if len(num) == 6:
+        if num not in answer:
+            answer.append(num)
+        return
+    
+    for dx, dy in delta:
+        next_x = x + dx
+        next_y = y + dy
+
+        if next_x < 0 or next_x > 4: continue
+        if next_y < 0 or next_y > 4: continue
+
+        dfs(next_x, next_y, num + board[next_x][next_y])
+
+
+for i in range(5):
+    for j in range(5):
+        dfs(i, j, board[i][j])
+print(len(answer))
