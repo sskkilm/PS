f76bb66cec9e679849874b089248ffcd255e2e0f
sskkilm <87752078+sskkilm@users.noreply.github.com>
2023-09-19 15:20:20 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.py
@@ -1,18 +1,10 @@
 def solution(s):
     answer = ""
+    arr = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     
-    my_dict = {"zero":'0', "one":'1', "two":'2', "three":'3', "four":'4', "five":'5', "six":'6', "seven":'7', "eight":'8', "nine":'9'}
+    for idx, val in enumerate(arr):
+        if val in s:
+            s = s.replace(val, str(idx))
+        answer = s
     
-    tmp = ""
-    for word in s:
-        if word.isdigit():
-            answer += word
-        else:
-            tmp += word
-            if tmp in my_dict:
-                answer += my_dict[tmp]
-                tmp = ""
-    
-    answer = int(answer)
-    
-    return answer
+    return int(answer)
