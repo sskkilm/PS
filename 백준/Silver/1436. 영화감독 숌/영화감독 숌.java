da389cbad040cefc84722e62045fdc25c67fbe08
sskkilm <87752078+sskkilm@users.noreply.github.com>
2024-01-18 19:15:48 +0900

    [Silver V] Title: 영화감독 숌, Time: 352 ms, Memory: 158556 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 1;
+        int num = 666;
+
+        while(N != cnt) {
+            num++;
+            if (String.valueOf(num).contains("666")) {
+                cnt++;
+            }
+        }
+        System.out.println(num);
+    }
+
+}
