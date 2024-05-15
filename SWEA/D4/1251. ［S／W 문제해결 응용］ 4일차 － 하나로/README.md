ff96a8939cb01c18152da29249c5176799fa1965
SeongGeun Lim <87752078+sskkilm@users.noreply.github.com>
2024-05-16 01:30:41 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 577 ms, Memory: 120,564 KB -BaekjoonHub

---
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/README.md
@@ -0,0 +1,15 @@
+# [D4] [S/W 문제해결 응용] 4일차 - 하나로 - 1251 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV15StKqAQkCFAYD) 
+
+### 성능 요약
+
+메모리: 120,564 KB, 시간: 577 ms, 코드길이: 4,544 Bytes
+
+### 제출 일자
+
+2024-05-16 01:30
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -0,0 +1,124 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;
+// double b = 1.0;
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Node {
+    int idx;
+    double cost;
+
+    public Node(int idx, double cost) {
+        this.idx = idx;
+        this.cost = cost;
+    }
+}
+
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+        //System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+
+            int N = sc.nextInt();
+            int[] x = new int[N];
+            int[] y = new int[N];
+            for (int i = 0; i < N; i++) {
+                x[i] = sc.nextInt();
+            }
+            for (int i = 0; i < N; i++) {
+                y[i] = sc.nextInt();
+            }
+            double E = sc.nextDouble();
+
+            List<List<Node>> graph = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                graph.add(new ArrayList<>());
+            }
+            for (int i = 0; i < N - 1; i++) {
+                for (int j = i + 1; j < N; j++) {
+                    double cost = (double) (x[i] - x[j]) * (x[i] - x[j]) + (double) (y[i] - y[j]) * (y[i] - y[j]);
+                    graph.get(i).add(new Node(j, E * cost));
+                    graph.get(j).add(new Node(i, E * cost));
+                }
+            }
+
+            PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Double.compare(o1.cost, o2.cost));
+            pq.add(new Node(0, 0));
+            boolean[] visited = new boolean[N];
+
+            double sum = 0;
+            int cnt = 0;
+            while (!pq.isEmpty()) {
+                Node node = pq.poll();
+
+                if (!visited[node.idx]) {
+                    visited[node.idx] = true;
+                    sum += node.cost;
+                    cnt++;
+                    if (cnt == N) {
+                        break;
+                    }
+                    for (Node next : graph.get(node.idx)) {
+                        if (!visited[next.idx]) {
+                            pq.add(next);
+                        }
+                    }
+                }
+            }
+
+            System.out.println(String.format("#%d %d", test_case, Math.round(sum)));
+        }
+    }
+}
