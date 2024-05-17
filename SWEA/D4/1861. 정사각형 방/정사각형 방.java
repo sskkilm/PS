47d5aa29fb60755ca0a2db695b2fdb7df27db36a
SeongGeun Lim <87752078+sskkilm@users.noreply.github.com>
2024-05-18 05:14:50 +0900

    [D4] Title: 정사각형 방, Time: 675 ms, Memory: 92,868 KB -BaekjoonHub

---
+++ SWEA/D4/1861. 정사각형 방/정사각형 방.java
@@ -0,0 +1,114 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static int N, max;
+	public static int[][] arr;
+	public static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+	public static boolean[][] visited;
+	
+	public static void dfs(int x, int y, int cnt) {
+		max = Math.max(max,  cnt);
+		
+		for (int[] dir : dirs) {
+			int nx = x + dir[0];
+			int ny = y + dir[1];
+			
+			if (nx < 0 || nx > N - 1) continue;
+			if (ny < 0 || ny > N - 1) continue;
+			
+			if (arr[nx][ny] == arr[x][y] + 1 && !visited[nx][ny]) {
+				visited[nx][ny] = true;
+				dfs(nx, ny, cnt + 1);
+				visited[nx][ny] = false;
+			}
+		}
+	}
+	public static void main(String args[]) throws Exception
+	{
+		/*
+		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
+		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
+		 */
+		//System.setIn(new FileInputStream("res/input.txt"));
+
+		/*
+		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			N = sc.nextInt();
+			arr = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = sc.nextInt();
+				}
+			}
+			
+			int[] ans = new int[N * N + 1];
+			visited = new boolean[N][N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					max = 0;
+					visited[i][j] = true;
+					dfs(i, j, 1);
+					visited[i][j] = false;
+					ans[arr[i][j]] = max;
+				}
+			}
+			
+			int m = ans[0];
+			int mi = 0;
+			for (int i = 1; i < ans.length; i++) {
+				if (ans[i] > m) {
+					m = ans[i];
+					mi = i;
+				}
+			}
+			System.out.println("#" + test_case + " " + mi + " " + m);
+		}
+	}
+}
