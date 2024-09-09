8c0ba92d8638c0cb4296bc0b2b12bbf579310d49
SeongGeun Lim <87752078+sskkilm@users.noreply.github.com>
2024-09-09 13:50:57 +0900

    [Silver IV] Title: 괄호, Time: 72 ms, Memory: 11744 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,36 +1,46 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = Integer.parseInt(sc.nextLine());
-        Deque<Character> stack = new ArrayDeque<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int T = Integer.parseInt(br.readLine());
+        Deque<Character> stack = null;
         for (int i = 0; i < T; i++) {
-            String str = sc.nextLine();
-
-            for (int j = 0; j < str.length(); j++) {
-                char c = str.charAt(j);
+            String s = br.readLine();
+            stack = new ArrayDeque<>();
+            boolean flag = true;
+            for (char c : s.toCharArray()) {
                 if (c == '(') {
-                    stack.add(c);
-                }
-                if (c == ')') {
-                    if (!stack.isEmpty()) {
-                        stack.pop();
-                    }
-                    else {
-                        stack.add(c);
+                    stack.push(')');
+                } else if (c == ')') {
+                    if (stack.isEmpty()) {
+                        flag = false;
                         break;
+                    } else {
+                        if (stack.peek() == c) {
+                            stack.pop();
+                        } else {
+                            flag = false;
+                            break;
+                        }
                     }
                 }
             }
-            if (stack.isEmpty()) {
+
+            if (flag && !stack.isEmpty()) {
+                flag = false;
+            }
+
+            if (flag) {
                 System.out.println("YES");
             } else {
                 System.out.println("NO");
             }
-
-            stack.clear();
         }
 
     }
