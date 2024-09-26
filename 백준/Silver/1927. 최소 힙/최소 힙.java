b1ab77135d02d03e26fac1f026eadc065b3dda09
SeongGeun Lim <87752078+sskkilm@users.noreply.github.com>
2024-09-26 12:51:21 +0900

    [Silver II] Title: 최소 힙, Time: 1056 ms, Memory: 34064 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -5,25 +5,20 @@ import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for (int i = 0; i < N; i++) {
-            if (arr[i] == 0) {
-                if (pq.isEmpty()) {
-                    System.out.println(0);
-                } else {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (!pq.isEmpty()) {
                     System.out.println(pq.poll());
+                } else {
+                    System.out.println(0);
                 }
             } else {
-                pq.add(arr[i]);
+                pq.add(x);
             }
         }
 
