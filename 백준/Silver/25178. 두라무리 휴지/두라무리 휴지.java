7cc6e8b3189530155fbc4dcc39920a80789812c5
SeongGeun Lim <87752078+sskkilm@users.noreply.github.com>
2024-10-02 19:07:42 +0900

    [Silver V] Title: 두라무리 휴지, Time: 236 ms, Memory: 29296 KB -BaekjoonHub

--- 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.java
+++ 백준/Silver/25178. 두라무리 휴지/두라무리 휴지.java
@@ -11,11 +11,11 @@ public class Main {
         String s1 = br.readLine();
         String s2 = br.readLine();
 
+        // 정렬하여 두 단어를 재배열했을 때 동일한지 확인
         char[] charArray1 = s1.toCharArray();
         char[] charArray2 = s2.toCharArray();
         Arrays.sort(charArray1);
         Arrays.sort(charArray2);
-
         for (int i = 0; i < N; i++) {
             if (charArray1[i] != charArray2[i]) {
                 System.out.println("NO");
@@ -23,11 +23,13 @@ public class Main {
             }
         }
 
+        // 두 문자열의 첫번째와 마지막이 일치하는지 확인
         if (s1.charAt(0) != s2.charAt(0) || s1.charAt(N - 1) != s2.charAt(N - 1)) {
             System.out.println("NO");
             return;
         }
 
+        // 모음을 제거했을 때 두 문자열이 일치하는지 확인
         String replacedS1 = s1
                 .replace("a", "")
                 .replace("e", "")
@@ -40,13 +42,12 @@ public class Main {
                 .replace("i", "")
                 .replace("o", "")
                 .replace("u", "");
-        for (int i = 0; i < replacedS1.length(); i++) {
-            if (replacedS1.charAt(i) != replacedS2.charAt(i)) {
-                System.out.println("NO");
-                return;
-            }
+        if (!replacedS1.equals(replacedS2)) {
+            System.out.println("NO");
+            return;
         }
 
+        // 모두 만족하는 경우
         System.out.println("YES");
     }
 }
