8b0de3403cbc3de3132369b69eeebc02b7162949
SeongGeun Lim <87752078+sskkilm@users.noreply.github.com>
2024-10-15 08:46:29 +0900

    [Gold V] Title: 탑, Time: 700 ms, Memory: 113964 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -11,35 +9,31 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N + 1];
+        int[] arr = new int[N];
+        int[] answer = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 1; i <= N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
-            map.put(arr[i], i);
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
         Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            int num = arr[i];
-            if (num > stack.peek()) {
-                while (!stack.isEmpty() && num > stack.peek()) {
-                    stack.pop();
-                }
-                if (stack.isEmpty()) {
-                    sb.append(0 + " ");
-                } else {
-                    sb.append(map.get(stack.peek()) + " ");
-                }
-                stack.push(num);
-            } else {
-                sb.append(map.get(stack.peek()) + " ");
-                stack.push(num);
+
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                stack.pop();
             }
+            if (!stack.isEmpty()) {
+                answer[i] = stack.peek() + 1; // 인덱스는 1부터 시작하므로 1을 더함
+            } else {
+                answer[i] = 0; // 수신하는 탑이 없을 경우 0
+            }
+            stack.push(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int res : answer) {
+            sb.append(res).append(" ");
         }
 
         System.out.println(sb);
