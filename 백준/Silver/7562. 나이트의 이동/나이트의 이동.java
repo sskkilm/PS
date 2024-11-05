b33bb87bc275c4558503c341c4fce65ea42ea61d
SeongGeun Lim <87752078+sskkilm@users.noreply.github.com>
2024-11-06 01:26:15 +0900

    [Silver I] Title: 나이트의 이동, Time: 224 ms, Memory: 63200 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -19,57 +19,53 @@ class Node {
 
 public class Main {
     public static int l;
-    public static int[][] arr;
-    public static int startX, startY, endX, endY;
-    public static boolean[][] visited;
-    public static int[][] dirs = {{-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {2, 1}, {1, 2}, {-1, 2}, {-2, 1}};
+    public static int[][] dirs = {{-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
             l = Integer.parseInt(br.readLine());
-            arr = new int[l][l];
 
             st = new StringTokenizer(br.readLine());
-            startX = Integer.parseInt(st.nextToken());
-            startY = Integer.parseInt(st.nextToken());
+            int st_x = Integer.parseInt(st.nextToken());
+            int st_y = Integer.parseInt(st.nextToken());
 
             st = new StringTokenizer(br.readLine());
-            endX = Integer.parseInt(st.nextToken());
-            endY = Integer.parseInt(st.nextToken());
+            int ed_x = Integer.parseInt(st.nextToken());
+            int ed_y = Integer.parseInt(st.nextToken());
 
-            visited = new boolean[l][l];
-            System.out.println(bfs());
+            bfs(st_x, st_y, ed_x, ed_y);
         }
     }
 
-    public static int bfs() {
+    public static void bfs(int st_x, int st_y, int ed_x, int ed_y) {
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(startX, startY, 0));
-        visited[startX][startY] = true;
+        queue.add(new Node(st_x, st_y, 0));
+
+        boolean[][] visited = new boolean[l][l];
+        visited[st_x][st_y] = true;
 
         while (!queue.isEmpty()) {
             Node cur = queue.poll();
-            if (cur.x == endX && cur.y == endY) {
-                return cur.len;
+            if (cur.x == ed_x && cur.y == ed_y) {
+                System.out.println(cur.len);
+                return;
             }
 
             for (int[] dir : dirs) {
-                int nx = cur.x + dir[0];
-                int ny = cur.y + dir[1];
+                int nx = dir[0] + cur.x;
+                int ny = dir[1] + cur.y;
 
                 if (nx < 0 || nx > l - 1) continue;
                 if (ny < 0 || ny > l - 1) continue;
 
                 if (!visited[nx][ny]) {
-                    queue.add(new Node(nx, ny, cur.len + 1));
                     visited[nx][ny] = true;
+                    queue.add(new Node(nx, ny, cur.len + 1));
                 }
             }
         }
 
-        return -1;
     }
 }
