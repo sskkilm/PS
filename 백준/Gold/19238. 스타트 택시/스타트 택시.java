7dcc144527ad1432ea10bc96e7c75c2cd2ceb9ab
SeongGeun Lim <87752078+sskkilm@users.noreply.github.com>
2024-12-26 00:26:36 +0900

    [Gold II] Title: 스타트 택시, Time: 272 ms, Memory: 25488 KB -BaekjoonHub

--- 백준/Gold/19238. 스타트 택시/스타트 택시.java
+++ 백준/Gold/19238. 스타트 택시/스타트 택시.java
@@ -15,61 +15,34 @@ class Passenger {
         this.ed_x = ed_x;
         this.ed_y = ed_y;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Passenger passenger = (Passenger) o;
-        return st_x == passenger.st_x && st_y == passenger.st_y && ed_x == passenger.ed_x && ed_y == passenger.ed_y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(st_x, st_y, ed_x, ed_y);
-    }
 }
 
-class PassengerNode {
+class PassengerInfo {
     Passenger passenger;
-    int len;
+    int distanceFromTaxi;
 
-    public PassengerNode(Passenger passenger, int len) {
+    public PassengerInfo(Passenger passenger, int distanceFromTaxi) {
         this.passenger = passenger;
-        this.len = len;
+        this.distanceFromTaxi = distanceFromTaxi;
     }
 }
 
 class Node {
     int x;
     int y;
-    int num;
 
-    public Node(int x, int y, int num) {
+    public Node(int x, int y) {
         this.x = x;
         this.y = y;
-        this.num = num;
-    }
-}
-
-class Node2 {
-    int x;
-    int y;
-    int len;
-
-    public Node2(int x, int y, int len) {
-        this.x = x;
-        this.y = y;
-        this.len = len;
     }
 }
 
 public class Main {
-    public static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
     public static int N;
     public static int[][] arr;
     public static int taxi_x, taxi_y;
-    public static List<Passenger> list = new ArrayList<>();
+    public static List<Passenger> passengers;
+    public static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -91,71 +64,70 @@ public class Main {
         taxi_x = Integer.parseInt(st.nextToken()) - 1;
         taxi_y = Integer.parseInt(st.nextToken()) - 1;
 
-        list = new ArrayList<>();
+        passengers = new ArrayList<>();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int st_x = Integer.parseInt(st.nextToken()) - 1;
             int st_y = Integer.parseInt(st.nextToken()) - 1;
             int ed_x = Integer.parseInt(st.nextToken()) - 1;
             int ed_y = Integer.parseInt(st.nextToken()) - 1;
-            list.add(new Passenger(st_x, st_y, ed_x, ed_y));
+            passengers.add(new Passenger(st_x, st_y, ed_x, ed_y));
         }
 
         for (int i = 0; i < M; i++) {
-
-            PassengerNode passengerNode = bfs();
-            if (passengerNode == null || passengerNode.len > fuel) {
+            PassengerInfo passengerInfo = findClosestPassengerInfo();
+            if (passengerInfo == null || passengerInfo.distanceFromTaxi > fuel) {
                 System.out.println(-1);
                 return;
             }
 
-            fuel -= passengerNode.len;
+            fuel -= passengerInfo.distanceFromTaxi;
 
-            Passenger passenger = passengerNode.passenger;
-            int len = bfs2(passenger.st_x, passenger.st_y, passenger.ed_x, passenger.ed_y);
-            if (len == -1 || len > fuel) {
+            Passenger passenger = passengerInfo.passenger;
+            int dist = getDistanceToEndPoint(passenger);
+            if (dist == -1 || dist > fuel) {
                 System.out.println(-1);
                 return;
             }
 
-            fuel += len;
+            fuel += dist;
 
             taxi_x = passenger.ed_x;
             taxi_y = passenger.ed_y;
-            list.remove(passenger);
+
+            passengers.remove(passenger);
         }
 
         System.out.println(fuel);
     }
 
-    private static PassengerNode bfs() {
-        PriorityQueue<PassengerNode> pq = new PriorityQueue<>((o1, o2) -> {
-            if (o1.len == o2.len && o1.passenger.st_x == o2.passenger.st_x) {
+    private static PassengerInfo findClosestPassengerInfo() {
+        PriorityQueue<PassengerInfo> pq = new PriorityQueue<>((o1, o2) -> {
+            if (o1.distanceFromTaxi == o2.distanceFromTaxi && o1.passenger.st_x == o2.passenger.st_x) {
                 return o1.passenger.st_y - o2.passenger.st_y;
             }
-            if (o1.len == o2.len) {
+            if (o1.distanceFromTaxi == o2.distanceFromTaxi) {
                 return o1.passenger.st_x - o2.passenger.st_x;
             }
 
-            return o1.len - o2.len;
+            return o1.distanceFromTaxi - o2.distanceFromTaxi;
         });
 
         Queue<Node> queue = new LinkedList<>();
-        boolean[][][] visited = new boolean[N][N][list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            Passenger passenger = list.get(i);
-            queue.add(new Node(passenger.st_x, passenger.st_y, i));
-            visited[passenger.st_x][passenger.st_y][i] = true;
-        }
+        queue.add(new Node(taxi_x, taxi_y));
+
+        boolean[][] visited = new boolean[N][N];
+        visited[taxi_x][taxi_y] = true;
 
         int level = 0;
         while (!queue.isEmpty()) {
             int size = queue.size();
             for (int i = 0; i < size; i++) {
                 Node cur = queue.poll();
-                if (cur.x == taxi_x && cur.y == taxi_y) {
-                    pq.add(new PassengerNode(list.get(cur.num), level));
-                    continue;
+                for (Passenger passenger : passengers) {
+                    if (cur.x == passenger.st_x && cur.y == passenger.st_y) {
+                        pq.add(new PassengerInfo(passenger, level));
+                    }
                 }
 
                 for (int[] dir : dirs) {
@@ -165,9 +137,9 @@ public class Main {
                     if (nx < 0 || nx > N - 1) continue;
                     if (ny < 0 || ny > N - 1) continue;
 
-                    if (arr[nx][ny] == 0 && !visited[nx][ny][cur.num]) {
-                        visited[nx][ny][cur.num] = true;
-                        queue.add(new Node(nx, ny, cur.num));
+                    if (arr[nx][ny] == 0 && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        queue.add(new Node(nx, ny));
                     }
                 }
             }
@@ -180,31 +152,36 @@ public class Main {
         return null;
     }
 
-    private static int bfs2(int st_x, int st_y, int ed_x, int ed_y) {
-        Queue<Node2> queue = new LinkedList<>();
-        queue.add(new Node2(st_x, st_y, 0));
+    private static int getDistanceToEndPoint(Passenger passenger) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(passenger.st_x, passenger.st_y));
 
         boolean[][] visited = new boolean[N][N];
-        visited[st_x][st_y] = true;
+        visited[passenger.st_x][passenger.st_y] = true;
 
+        int level = 0;
         while (!queue.isEmpty()) {
-            Node2 cur = queue.poll();
-            if (cur.x == ed_x && cur.y == ed_y) {
-                return cur.len;
-            }
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                if (cur.x == passenger.ed_x && cur.y == passenger.ed_y) {
+                    return level;
+                }
 
-            for (int[] dir : dirs) {
-                int nx = cur.x + dir[0];
-                int ny = cur.y + dir[1];
+                for (int[] dir : dirs) {
+                    int nx = cur.x + dir[0];
+                    int ny = cur.y + dir[1];
 
-                if (nx < 0 || nx > N - 1) continue;
-                if (ny < 0 || ny > N - 1) continue;
+                    if (nx < 0 || nx > N - 1) continue;
+                    if (ny < 0 || ny > N - 1) continue;
 
-                if (arr[nx][ny] == 0 && !visited[nx][ny]) {
-                    visited[nx][ny] = true;
-                    queue.add(new Node2(nx, ny, cur.len + 1));
+                    if (arr[nx][ny] == 0 && !visited[nx][ny]) {
+                        visited[nx][ny] = true;
+                        queue.add(new Node(nx, ny));
+                    }
                 }
             }
+            level++;
         }
 
         return -1;
